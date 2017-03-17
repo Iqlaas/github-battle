10d120bc66cc5af8a27ae3a59db44d2eb21cd617
Iqlaas <kamaliqlaas@gmail.com>
2017-03-17 12:27:24 +0800

    done github battle

---
+++ notes.js
@@ -0,0 +1,20 @@
+/*
+- implicit binding
+- explicit binding
+- new binding
+- window binding
+*/
+
+// where is the function invoked ?
+
+// implicit binding
+// left of the dot at call time
+
+// explicit binding
+// call, apply, bind
+
+// new binding
+
+
+
+// window binding
